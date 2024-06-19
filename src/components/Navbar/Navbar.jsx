b4dd0d0f48aec8c3e6935a9a1de4603f228a9614
Mohammed Yeasin <yeasinbinali@gmail.com>
2024-06-19 20:39:30 +0600

    navbar added

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,54 @@
+import Link from 'next/link';
+import React from 'react';
+// import logo from '../../assets/logo.png'; 
+
+const Navbar = () => {
+    const navItems = [
+        {
+            title: 'Home',
+            location: '/'
+        },
+        {
+            title: 'About',
+            location: '/about'
+        },
+        {
+            title: 'Contact',
+            location: '/contact'
+        }
+    ];
+
+    return (
+        <div className="navbar bg-base-100">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </div>
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                        {navItems.map(nav => (
+                            <li key={nav.title}>
+                                <Link href={nav.location}>{nav.title}</Link>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+                <h1 className='text-main text-4xl font-bold'>Yeasin</h1>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    {navItems.map(nav => (
+                        <li key={nav.title} className='text-lg'>
+                            <Link href={nav.location}>{nav.title}</Link>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            <div className="navbar-end">
+                <a className="btn">Resume</a>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        main: '#003285'
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
