6cf68e6ab6ca139548af170ce217136bc197edc1
Mohammed Yeasin <yeasinbinali@gmail.com>
2024-06-21 00:12:19 +0600

    technologies section added

--- src/components/Technology/Technology.jsx
+++ src/components/Technology/Technology.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
 
 const Technology = () => {
+    const [technologies, setTechnologies] = useState([]);
+
+    axios.get('./tech.json')
+        .then(res => {
+            setTechnologies(res.data);
+        })
+
     return (
         <div className='mb-20'>
             <h1 className='text-4xl font-bold text-main mb-10'>Technologies I'v been working on __</h1>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10'>
+                {technologies.map(technology => <div key={technology.id} className='flex items-center bg-sky-50 justify-center p-5 transition duration-300 ease-in-out hover:scale-110'>
+                    <img className='w-10 h-10' src={technology.image} alt="tech" />
+                    <h3 className='text-xl ml-5 font-medium'>{technology.name}</h3>
+                </div>)}
+            </div>
         </div>
     );
 };
