a2db0a83d28899579bd8ca91d007bef433a5b3c1
Mohammed Yeasin <yeasinbinali@gmail.com>
2024-06-22 14:21:26 +0600

    all projects section added

--- src/components/AllProjects/AllProjects.jsx
+++ src/components/AllProjects/AllProjects.jsx
@@ -2,8 +2,6 @@ import React, { useEffect, useState } from 'react';
 import HeaderName from '../HeaderName/HeaderName';
 import axios from 'axios';
 import Project from '../Project/Project';
-import Button from '../Button/Button';
-import Link from 'next/link';
 
 const Projects = () => {
     const [projects, setProjects] = useState([]);
@@ -19,9 +17,8 @@ const Projects = () => {
         <div className='mb-20'>
             <HeaderName name={"Some thing I've built"}></HeaderName>
             <div>
-                {projects.slice(0, 3).map(project => <Project key={project.id} project={project}></Project>)}
+                {projects.map(project => <Project key={project.id} project={project}></Project>)}
             </div>
-            <Link href={"All Projects"}><Button name={"View More Projects"}></Button></Link>
         </div>
     );
 };
